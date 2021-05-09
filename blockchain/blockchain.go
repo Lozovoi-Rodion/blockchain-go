ccc449de0a436f8b4befc78cc36f70ae94b0343f
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-05-09 23:44:19 +0300

    finishing badger integration. [WIP]

---
+++ blockchain/blockchain.go
@@ -0,0 +1,107 @@
+package blockchain
+
+import (
+	"fmt"
+	"github.com/dgraph-io/badger"
+)
+
+type BlockChain struct {
+	LastHash []byte
+	Database *badger.DB
+}
+
+type Iterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
+const dbPath = "./tmp/blocks"
+
+func InitBlockChain() *BlockChain {
+	var lastHash []byte
+
+	opts := badger.DefaultOptions(dbPath)
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.ValueCopy(nil)
+			return err
+		}
+	})
+
+	Handle(err)
+
+	blockchain := BlockChain{
+		LastHash: lastHash,
+		Database: db,
+	}
+	return &blockchain
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.ValueCopy(nil)
+		return err
+	})
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err = txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
+}
+
+func (chain BlockChain) Iterator() *Iterator {
+	iter := &Iterator{
+		CurrentHash: chain.LastHash,
+		Database:    chain.Database,
+	}
+
+	return iter
+}
+
+func (iter *Iterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		Handle(err)
+		encodedBlock, err := item.ValueCopy(nil)
+		block = Deserialize(encodedBlock)
+
+		return err
+	})
+	Handle(err)
+
+	iter.CurrentHash = block.PrevHash
+
+	return block
+}
